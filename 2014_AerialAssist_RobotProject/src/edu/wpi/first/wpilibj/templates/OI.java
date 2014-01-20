9f7ec2002efa9ed760b3983b565812079bb5dc23
Chris Long <chlong@umflint.edu>
2014-01-20 18:03:31 -0500

    OI Updated, Very Dynamic Methods

--- 2014_AerialAssist_RobotProject/src/edu/wpi/first/wpilibj/templates/OI.java
+++ 2014_AerialAssist_RobotProject/src/edu/wpi/first/wpilibj/templates/OI.java
@@ -1,44 +1,177 @@
-
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.DigitalIOButton;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
+
+    // Create the joystick and of the 8 buttons on it
+    Joystick driverControllerXbox = new Joystick(1);
+        Button xButton_driverControllerXbox = new JoystickButton(driverControllerXbox, 1),
+                yButton_driverControllerXbox = new JoystickButton(driverControllerXbox, 2),
+                bButton_driverControllerXbox = new JoystickButton(driverControllerXbox, 3),
+                aButton_driverControllerXbox = new JoystickButton(driverControllerXbox, 4),
+                startButton_driverControllerXbox = new JoystickButton(driverControllerXbox, 5),
+                backButton_driverControllerXbox = new JoystickButton(driverControllerXbox, 6),
+                leftBumperButton_driverControllerXbox = new JoystickButton(driverControllerXbox, 7),
+                rightBumperButton_driverControllerXbox = new JoystickButton(driverControllerXbox, 8),
+                leftJoystickButton_driverControllerXbox = new JoystickButton(driverControllerXbox, 9),
+                rightJoystickButton_driverControllerXbox = new JoystickButton(driverControllerXbox, 8);
+  
+    Joystick operatorControllerXbox = new Joystick(2);
+        Button xButton_operatorControllerXbox = new JoystickButton(operatorControllerXbox, 1),
+                yButton_operatorControllerXbox = new JoystickButton(operatorControllerXbox, 2),
+                bButton_operatorControllerXbox = new JoystickButton(operatorControllerXbox, 3),
+                aButton_operatorControllerXbox = new JoystickButton(operatorControllerXbox, 4),
+                startButton_operatorControllerXbox = new JoystickButton(operatorControllerXbox, 5),
+                backButton_operatorControllerXbox = new JoystickButton(operatorControllerXbox, 6),
+                leftBumperButton_operatorControllerXbox = new JoystickButton(operatorControllerXbox, 7),
+                rightBumperButton_operatorControllerXbox = new JoystickButton(operatorControllerXbox, 8),
+                leftJoystickButton_operatorControllerXbox = new JoystickButton(operatorControllerXbox, 9),
+                rightJoystickButton_operatorControllerXbox = new JoystickButton(operatorControllerXbox, 8);
     
-    // Another type of button you can create is a DigitalIOButton, which is
-    // a button or switch hooked up to the cypress module. These are useful if
-    // you want to build a customized operator interface.
-    // Button button = new DigitalIOButton(1);
+    Joystick driverControllerJoystick = new Joystick(1);
+        Button triggerButton_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 1),
+                thumbButtonNum_2_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 2),
+                thumbButtonNum_3_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 3),
+                thumbButtonNum_4_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 4),
+                thumbButtonNum_5_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 5),
+                thumbButtonNum_6_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 6),
+                baseButtonNum_7_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 7),
+                baseButtonNum_8_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 8),
+                baseButtonNum_9_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 9),
+                baseButtonNum_10_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 10),
+                baseButtonNum_11_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 11),
+                baseButtonNum_12_driverControllerJoystick = new JoystickButton(driverControllerJoystick, 12);
+
+    Joystick operatorControllerJoystick = new Joystick(2);
+        Button triggerButton_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 1),
+                thumbButtonNum_2_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 2),
+                thumbButtonNum_3_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 3),
+                thumbButtonNum_4_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 4),
+                thumbButtonNum_5_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 5),
+                thumbButtonNum_6_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 6),
+                baseButtonNum_7_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 7),
+                baseButtonNum_8_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 8),
+                baseButtonNum_9_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 9),
+                baseButtonNum_10_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 10),
+                baseButtonNum_11_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 11),
+                baseButtonNum_12_operatorControllerJoystick = new JoystickButton(operatorControllerJoystick, 12);     
+    /**
+     * Bind the press of each button to a specific command or command group.
+     * @param controller - Driver or Operator
+     * @param side - Left or Right
+     * @return zero if no cases meet requirements 
+     */
+    public double getTrigger(int controller, int side)
+    {
+        switch (controller) 
+        {
+           case 1:
+               if(side == 0)
+               {
+                    if(driverControllerXbox.getRawAxis(3) < 0)
+                    { 
+                        return driverControllerXbox.getRawAxis(3);
+                    }
+               }
+               if(side == 1)
+               {
+                    if(driverControllerXbox.getRawAxis(3) > 0)
+                    { 
+                        return driverControllerXbox.getRawAxis(3);
+                    }
+               }
+               break;
+           case 2:
+               if(side == 0)
+               {
+                    if(operatorControllerXbox.getRawAxis(3) < 0)
+                    { 
+                        return operatorControllerXbox.getRawAxis(3);
+                    }  
+               }
+               if(side == 1)
+               {
+                    if(operatorControllerXbox.getRawAxis(3) > 0)
+                    { 
+                        return operatorControllerXbox.getRawAxis(3);
+                    }    
+               }
+               break;
+           default:
+               break;
+                   
+        }   
+        return 0;
+    }
     
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
+    getJoystick(2,0,1)
+            
+    getJoystick(DriverControllerXbox, Left, X);
     
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
+    public double getJoystick(int controller, int side, int axis)
+    {
+        switch (controller)
+        {
+            case 1: // Driver Controller
+                if(side == 0)   // Left side
+                {
+                    if(axis == 0)   // X Axis
+                    {
+                        return driverControllerXbox.getRawAxis(1);
+                    }
+                    if(axis == 1) // Y Axis
+                    {
+                        return driverControllerXbox.getRawAxis(2);                 
+                    }
+                }
+                if(side == 1)   // Right Side
+                {
+                    if(axis == 0)   // X Axis
+                    {
+                        return driverControllerXbox.getRawAxis(4);
+                    }
+                    if(axis == 1) // Y Axis
+                    {
+                        return driverControllerXbox.getRawAxis(5);                 
+                    }
+                }
+                break;
+            case 2: // Operator Controller
+                if(side == 0)   // Left side
+                {
+                    if(axis == 0)   // X Axis
+                    {
+                        return operatorControllerXbox.getRawAxis(1);
+                    }
+                    if(axis == 1) // Y Axis
+                    {
+                        return operatorControllerXbox.getRawAxis(2);                 
+                    }
+                }
+                if(side == 1)   // Right Side
+                {
+                    if(axis == 0)   // X Axis
+                    {
+                        return operatorControllerXbox.getRawAxis(4);
+                    }
+                    if(axis == 1) // Y Axis
+                    {
+                        return operatorControllerXbox.getRawAxis(5);                 
+                    }
+                }
+                break;
+            default:
+               break;            
+        }
+        return 0;
+        
+    }    
 }
 
